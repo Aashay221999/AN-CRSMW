7574e77b6573d4c3c5a49114747de10391ec4ef9
Shannon Vaz <nonnahs.vaz@gmail.com>
2023-02-24 21:57:15 +0530

    Update example routes

--- api/routes/index.js
+++ api/routes/index.js
@@ -5,14 +5,7 @@ const exampleRoutes = require("./routes.example.js");
 
 const router = express.Router();
 
-/**
- * Add example routes to app
- */
+// Add example routes to app
 router.use("/example", exampleRoutes);
-
-// Apply routes on app
-// app.use(identityRoutes)
-// app.use(claimRequestRoutes)
-// app.use(signedClaimsRoutes)
 
 module.exports = router;
